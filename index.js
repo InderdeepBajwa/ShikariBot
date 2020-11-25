6093d11ffaa68da354c2afbd0217b6dbe354fc36
unknown <inderdeepbajwa@users.noreply.github.com>
2020-11-25 05:37:06 -0500

    Setting up nodemon

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // Library requirements
-import * as dotenv from 'dotenv';
+const dotenv = require('dotenv');
+dotenv.config();
 
-dotenv.config()
+// Importing Express (web server)
 
+const express = require('express');
+const app = express();
+const port = process.env.WEB_SERVER_PORT;
 
+app.get('/', (req, res) => {
+    res.send('Shikari Bot Hello');
+});
+
+app.listen(port, () => {
+    console.log(`Listening at ${port}`);
+});
